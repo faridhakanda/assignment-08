9e4b42e49812253461a0f31a912d7d5d69d226a8
faridhakanda <faridakanda1729@gmail.com>
2026-04-30 11:49:07 +0600

    login file updated

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 const LoginPage = () => {
+    const [isShow, setIsShow] = useState(false);
     return (
         <div>
             <h2>Login Page</h2>
+            <div className='flex bg-slate-200 p-1 m-1'>
+                <input 
+                    type="text"
+                    className='flex'
+                    placeholder='Enter your email'
+                />
+                <input 
+                    className='flex'
+                    type={isShow ? "text": "password"}
+                    placeholder='Enter your password'
+                />
+
+            </div>
         </div>
     );
 };
