e6a2e702d16042685e3e8c35e8700a4bd7152632
faridhakanda <faridakanda1729@gmail.com>
2026-05-01 23:25:08 +0600

    header and navbar is almost done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,15 +19,33 @@ const Navbar = () => {
             <div className='visible md:hidden'>
                 <div className='bg-slate-50 flex justify-between items-center p-2 m-1 shadow-sm'>
                     <h2>Mobile Navbar</h2>
-                    <div>{isShow ? <span className='cursor-pointer' onClick={() => setIsShow(!isShow)}><IoIosMenu /></span> : <span className='cursor-pointer' onClick={() => setIsShow(!isShow)}><RxCross1 /></span>}</div>
+                    <div
+                        onClick={() => setIsShow(!isShow)}
+                    >
+                        {isShow ? <IoIosMenu /> : <RxCross1 />}
+                        
+                    </div>
                 </div>
                 {!isShow && 
                     <div className='w-64 opacity-100 z-100 px-4 py-4 m-1 bg-slate-50 justify-start'>
-                        <NavLink className="py-1"  href="/">Home</NavLink>
-                        <NavLink className="py-1" href="/login">Login</NavLink>
-                        <NavLink className="py-1" href="/register">Register</NavLink>
-                        <NavLink className="py-1" href="/notfound">NotFound</NavLink>
-                        {mobile ? <NavLink href="/myprofile">MyProfile</NavLink> : <NavLink href="/logout">Logout</NavLink>}
+                        <div onClick={() => setIsShow(true)}>
+                            <NavLink  className="py-1"  href="/">Home</NavLink>
+                        </div>
+                        <div onClick={() => setIsShow(true)}>
+                            <NavLink  className="py-1" href="/login">Login</NavLink>
+                        </div>
+                        <div onClick={() => setIsShow(true)}>
+                            <NavLink  className="py-1" href="/register">Register</NavLink>
+                        </div>
+                        <div onClick={() => setIsShow(true)}>
+                            <NavLink  className="py-1" href="/notfound">NotFound</NavLink>
+                        </div>
+                       
+                        <div onClick={() => setIsShow(true)}>
+                            {mobile ? <NavLink  href="/myprofile">MyProfile</NavLink> : <NavLink href="/logout">Logout</NavLink>}
+                        </div>
+                        
+                       
                     </div>
                 }
             </div>
